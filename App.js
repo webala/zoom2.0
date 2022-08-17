4f06f16574f64d01a8fe32edd0536c4f5b8b1cd6
webala <webdspam@gmail.com>
2022-08-17 19:54:40 +0300

    ADD: Navigation

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home'
+import Navigation from './Navigation';
 
 export default function App() {
   return (
-    <View>
-      <Home />
-    </View>
+    <Navigation />
   );
 }
 
---
+++ screens/MeetingRoom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function MeetingRoom() {
+  return (
+    <View>
+        <Text>MeetingRoom</Text>
+    </View>
+  )
+}
+
+export default MeetingRoom
